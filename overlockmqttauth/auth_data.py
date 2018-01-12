89aa689160d5ce5b586566e96f26845afe7691ca
Michael Boulton <boulton@zoetrope.io>
2018-01-12 16:05:41 +0000

    Add initial stuff for verifying connections

---
+++ overlockmqttauth/auth_data.py
@@ -0,0 +1,27 @@
+class Secret:
+    """Represents a device or project secret
+    """
+    def __init__(self, val, blacklisted):
+        self.val = val
+        self.blacklisted = blacklisted
+
+
+projects = [
+    {
+        "name": "abc123",
+        "secrets": [
+            Secret("sdkg40wkgk3pok32", False),
+            Secret("bm,vbmvbmvbcmmbn", False),
+        ],
+        "blacklisted": False,
+    },
+]
+
+
+devices = [
+    {
+        "project": "abc123",
+        "device": "blapt",
+        "secret": Secret("2123122413423", False),
+    },
+]
