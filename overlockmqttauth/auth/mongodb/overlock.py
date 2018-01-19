5a40c8bd38098fb9924c5ccc79c08209c95fd014
Michael Boulton <boulton@zoetrope.io>
2018-01-19 17:24:37 +0000

    dont need product model

--- overlockmqttauth/auth/mongodb/overlock.py
+++ overlockmqttauth/auth/mongodb/overlock.py
@@ -1,10 +1,6 @@
 import mongoengine
 
 
-class Product(mongoengine.Document):
-    iot_name = mongoengine.StringField()
-
-
 class Project(mongoengine.Document):
     name = mongoengine.StringField(required=True)
     project_keys = mongoengine.ListField(mongoengine.StringField())
